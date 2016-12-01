0b82732f15ec70e50c134f55f801d493bd5c89d5
greicens <gsilva@mail.ccsf.edu>
2016-11-30 23:37:06 -0800

    added a few prompt alerts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,9 @@
 // sanity check
 console.log("app.js linked.");
+
+window.alert("Boring Content enter at your own risk!");
+var userResponse = window.prompt("Are you in WDI-34?");
+
+if(userResponse.toLowerCase() === "yes"){
+  window.alert("Feel Free to email me with suggestions.")
+}
