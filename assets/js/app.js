7af97d048c97ba73c12934eb389eeeaa3cedf0a6
greicens <gsilva@mail.ccsf.edu>
2016-12-01 23:47:45 -0800

    added a jQuery event listener

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,18 @@ var userResponse = window.prompt("Are you in WDI-34?");
 if(userResponse.toLowerCase() === "yes"){
   window.alert("Feel Free to email me with suggestions.")
 }
+$(document).ready(function(){
+    // code in here DOES wait for DOM to be ready
+    // best place for DOM element selectors
+    // best place to *call* functions that interact with the DOM
+    var greeting = $('#alert');
+    greeting.on('mouseover', popUpYay);
+});
+
+// code down here does not wait for DOM to be ready
+// great place to define functions, even if they interact with the DOM
+function popUpYay(event){
+    alert('Yay!');
+    // DOM interaction is planned here - but only gets executed when function is called
+    $('body').append('Yay!');
+}
