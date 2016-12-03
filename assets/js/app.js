286124c127f96d19f4a3cd109e6bf60091570b9d
greicens <gsilva@mail.ccsf.edu>
2016-12-02 23:09:07 -0800

    removed a few popups

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,8 @@
 // sanity check
 console.log("app.js linked.");
 
-window.alert("Boring Content enter at your own risk!");
-var userResponse = window.prompt("Are you in WDI-34?");
+window.alert("Please hoover mouse over Contacts to see a jQuery event in action")
 
-if(userResponse.toLowerCase() === "yes"){
-  window.alert("Please hoover mouse over Contacts to see a jQuery event in action")
-}
 
 var cheers = '<iframe width="560" height="315" src="https://www.youtube.com/embed/barWV7RWkq0" frameborder="0" allowfullscreen></iframe>';
 
@@ -15,13 +11,13 @@ $(document).ready(function(){
     // best place for DOM element selectors
     // best place to *call* functions that interact with the DOM
     var greeting = $('#alert');
-    greeting.on('mouseover', popUpYay);
+    greeting.on('mouseover', popUp);
 });
 
 // code down here does not wait for DOM to be ready
 // great place to define functions, even if they interact with the DOM
-function popUpYay(event){
+function popUp(event){
     alert('It worked !');
     // DOM interaction is planned here - but only gets executed when function is called
-    $('body').append('Yay!'); 
+    $('body').append('Yay!');
 }
